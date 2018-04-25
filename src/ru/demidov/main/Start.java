b560f52813dafd77401037205b041b4956ba8cb5
Kaban77 <demidovmax4@gmail.com>
2018-04-25 12:23:58 +0300

    Algorithm changed

--- src/ru/demidov/main/Start.java
+++ src/ru/demidov/main/Start.java
@@ -1,18 +1,18 @@
 package ru.demidov.main;
 
-import ru.demidov.interfaces.Table;
 import ru.demidov.objects.TableImpl;
 
 public class Start {
 
     public static void main(String[] args) {
-        Table table = new TableImpl(10, 10);
+        var table = new TableImpl(10, 10);
 
         try {
-            table.put(1, 1, "TEST");
+            System.out.println(table.put(1, 1, "TEST"));
+            System.out.println(table.put(1, 1, "TEST2"));
 
-            System.out.println(table.get(1, 11));
-            System.out.println(table.get(0, 1));
+            System.out.println(table.get(2, 1));
+            System.out.println(table.get(1, 1));
         } catch (Exception e) {
             e.printStackTrace();
         }
